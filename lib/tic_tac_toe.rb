feb3ed0481debc2670e8c185d3dce62ee777c63c
Philip Warren <philwarren1@gmail.com>
2020-08-10 18:11:20 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,6 +29,6 @@ class TicTacToe
   @board[location] != " " && @board[location] != ""
  end
  def valid_move?(index)
-  index.between?(0,8) && !position_taken?(@board, index)
+  index.between?(0,8) && !position_taken?(index)
 end
 end
