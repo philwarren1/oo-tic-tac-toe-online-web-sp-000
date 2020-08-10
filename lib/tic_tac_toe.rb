2f4b1bf93f87c21368f15f2ce3ea0704b5cd1c35
Philip Warren <philwarren1@gmail.com>
2020-08-10 18:13:57 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,5 +31,7 @@ class TicTacToe
  def valid_move?(index)
   index.between?(0,8) && !position_taken?(index)
 end
- 
+ def turn_count
+    @board.count{|token| token == "X" || token == "O"}
+  end
 end
