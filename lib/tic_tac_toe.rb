f9a0cf234bf1ea8ef9c5ca6fe42ad5eac301b8bd
Philip Warren <philwarren1@gmail.com>
2020-08-10 17:56:37 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,5 +2,14 @@ class TicTacToe
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
-  
+WIN_COMBINATIONS = [ 
+ [0,1,2], # top_row 
+ [3,4,5], # middle_row 
+ [6,7,8], # bottom_row 
+ [0,3,6], # left_column 
+ [1,4,7], # center_column 
+ [2,5,8], # right_column 
+ [0,4,8], # left_diagonal 
+ [6,4,2] # right_diagonal 
+ ]
 end
